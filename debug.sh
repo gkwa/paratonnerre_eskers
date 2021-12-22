89da65b47dc42c0545812aa3a117074ceee71d80
Your Name <you@example.com>
2021-12-22 14:27:42 -0500

    real men dont make git commit comments

--- debug.sh
+++ debug.sh
@@ -3,6 +3,5 @@
 ls -la /opt/paratonnerre_eskers/*
 ls -la /var/log/paratonnerre_eskers/*
 ls -la /home/centos/.config/autostart/*.sh.desktop
-ls -la /usr/local/bin/popup.sh
 
 grep paratonnerre_eskers /etc/crontab
