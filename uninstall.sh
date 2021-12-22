3ecb0b323db0e67dbf611ae85f754ede031d71c6
Your Name <you@example.com>
2021-12-21 18:38:56 -0800

    real men dont make git commit comments

--- uninstall.sh
+++ uninstall.sh
@@ -9,3 +9,4 @@ sudo rm -f /usr/local/bin/popup.sh
 sudo rm -f /home/centos/.config/autostart/popup.sh.desktop
 sudo rm -f /etc/logrotate.d/paratonnerre_eskers
 sudo rm -rf /opt/paratonnerre_eskers/
+
