52bd03ecaf62a02e6980a93d0b63a1d4e50b8a2a
Your Name <you@example.com>
2021-12-21 18:14:06 -0800

    real men dont make git commit comments

--- shutdown_timeout.sh
+++ shutdown_timeout.sh
@@ -11,12 +11,8 @@ start_timer() {
     i=0
     while sleep 36; do
         echo $((i++))
-        date +%s >>/var/log/paratonnerre_eskers/lastrun_timestamp.log
-    done | zenity --progress \
-        --text="To conserve resources we will auto-shutdown this host in an hour. To extend your time for another hour, cancel this popup." \
-        --title="Autoshutdown in 1h" \
-        --width=300 --height=200 \
-        --auto-close
+    done | zenity --progress --width=300 --height=200 --title="Autoshutdown in 1h" --auto-close \
+        --text="To conserve resources we will auto-shutdown this host in an hour. To extend your time for another hour, cancel this popup."
     retVal=$?
 
     if [ $retVal -ne 0 ]; then
