084c78954248db3dd7ee537111186398b1ce67b6
Your Name <you@example.com>
2021-12-21 18:03:41 -0800

    real men dont make git commit comments

--- shutdown_timeout.sh
+++ shutdown_timeout.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+write_timestamp() {
+    echo "I'm still here!"
+    d1=$(date +%s -d "+1 hour")
+    d2=$(date -d "+1 hour")
+    echo "$d1 # $d2" >>/var/log/paratonnerre_eskers/shutdown.log
+}
+
 start_timer() {
     i=0
     while sleep 36; do
@@ -13,16 +20,18 @@ start_timer() {
     retVal=$?
 
     if [ $retVal -ne 0 ]; then
-        echo "I'm still here!"
-        d1=$(date +%s -d "+1 hour")
-        d2=$(date     -d "+1 hour")
-        echo "$d1 # $d2" >>/var/log/paratonnerre_eskers/shutdown.log
+        write_timestamp
     else
         echo "I've moved on"
     fi
 }
 
-while true; do
-    start_timer
-    sleep 1m
-done
+main() {
+    write_timestamp
+    while true; do
+        start_timer
+        sleep 1m
+    done
+}
+
+main
