e5808f8ac9d59922e654cb815a258f8bbcc793cf
Your Name <you@example.com>
2021-12-29 13:26:48 -0500

    Dont' show popup unitl its time to shutdown and then allow 20 minutes to cancel shutdown

--- popup.sh
+++ popup.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
 . /opt/paratonnerre_eskers/common.sh
+CANCEL_TIME=20 #MINUTES
 
 start_timer() {
+    sleep 50m
+
     i=0
-    while sleep 36; do
+    while sleep $(($CANCEL_TIME*60/100))s; do
         echo $((i++))
-    done | zenity --progress --width=300 --height=200 --title="Autoshutdown in 1h" --auto-close \
-        --text="To conserve resources we will auto-shutdown this host in an hour. To extend your time for another hour, cancel this popup."
+    done | zenity --progress --width=300 --height=200 --title="Autoshutdown in $CANCEL_TIME minutes" --auto-close \
+        --text="To conserve resources we will auto-shutdown this host in $CANCEL_TIME minutes. To extend your time for another hour, cancel this popup."
     retVal=$?
 
     if [ $retVal -ne 0 ]; then
